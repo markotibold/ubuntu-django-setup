bb4ac8f88fe232607e995a01a7b949ae46c2abfa
Marko Tibold <marko@tibold.nl>
2012-11-25 15:10:15 +0100

    Update install.sh
    Add fsm alias

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
 echo 'export LC_CTYPE="en_US.UTF-8"' >> ~/.bashrc
 echo 'export WORKON_HOME=~/envs' >> ~/.bashrc
 echo 'alias rs="python manage.py runserver 0:8000"' >> ~/.bashrc
-
+echo 'alias fsm="fab staging media && tar -xvf project_media.tar"' >> ~/.bashrc
